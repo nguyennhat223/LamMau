706754512b0ae70980ede0f41fb2638c4ce10edb
Nguyen Tan Nhat <nguyentannhat42@gmail.com>
2025-07-01 21:16:03 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -190,6 +190,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
+	base: '/LamMau/',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin()] : []),
